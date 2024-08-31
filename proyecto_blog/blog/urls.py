394168e2dfdbeac8b2eee6dab788bba9fe4693d5
Mayderking <163465498+Mayderking@users.noreply.github.com>
2024-08-31 15:53:21 -0500

    avance 3
    se creo un filtro de busqueda por fecha, la vista de detalle de una publicacion y la posibilidad de editar las publicaciones

--- proyecto_blog/blog/urls.py
+++ proyecto_blog/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ver_lista_publicaciones, agregar_publicacion
+from .views import ver_lista_publicaciones, agregar_publicacion, editar_publicacion,detalles_publicacion
 
 urlpatterns = [
     path('posts/', ver_lista_publicaciones, name='ver_lista_publicaciones'),
     path('agregar_publicacion/', agregar_publicacion, name='agregar_publicacion'),
+    path('editar_publicacion/<int:post_id>', editar_publicacion, name='editar_publicacion'),
+    path('detalles_publicacion<int:post_id>/', detalles_publicacion, name='detalles_publicacion'),
 ]
--- proyecto_blog/blog/views.py
+++ proyecto_blog/blog/views.py
@@ -1,17 +1,34 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostModelForm
 from django.db.models import Q
 from django.core.paginator import Paginator
+from datetime import datetime
 
 # Create your views here.
 
 def ver_lista_publicaciones(request):
     query = request.GET.get('q')
+    inicio_fecha = request.GET.get('inicio_fecha')
+    final_fecha = request.GET.get('final_fecha')
     posts_list = Post.objects.all()
 
     if query:
         posts_list = posts_list.filter(Q(titulo__icontains=query)| Q(contenido__icontains=query)| Q(categoria__icontains=query))
+
+    if inicio_fecha:
+        try:
+            inicio_fecha = datetime.strptime(inicio_fecha, '%Y-%m-%d')
+            posts_list = posts_list.filter(fecha_publicacion__gte=inicio_fecha)
+        except ValueError:
+            pass
+
+    if final_fecha:
+        try:
+            final_fecha = datetime.strptime(final_fecha, '%Y-%m-%d')
+            posts_list = posts_list.filter(fecha_publicacion__lte=final_fecha)
+        except ValueError:
+            pass
 
     paginator = Paginator(posts_list, 10)
     page_number = request.GET.get('page')
@@ -31,3 +48,18 @@ def agregar_publicacion(request):
     else:
         form = PostModelForm
     return render(request, 'agregar_publicacion.html', {'form': form})
+
+def editar_publicacion(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        form = PostModelForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('ver_lista_publicaciones')
+    else:
+        form = PostModelForm(instance=post)
+    return render(request, 'editar_publicacion.html', {'form': form, 'post': post})
+
+def detalles_publicacion(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'detalles_publicacion.html', {'post': post})
